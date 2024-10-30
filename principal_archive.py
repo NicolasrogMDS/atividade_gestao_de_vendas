4197b8392b3f23f6226f607fd8ce158b7e09c5fa
Nicolas Roger <nramonteirods05@gmail.com>
2024-10-30 17:17:19 +0000

    database_principal_archive

--- principal_archive.py
+++ principal_archive.py
@@ -5,3 +5,33 @@ Júlia Vitória
 Heloísa Lima
 Nicolas Roger
 """
+
+from os import system
+from time import sleep
+from sqlalchemy import create_engine, Column, String, Integer, Float
+from sqlalchemy.orm import sessionmaker, declarative_base 
+
+dados = []
+#Criando Banco de dados:
+BD = create_engine("sqlite:///sistema_de_gestao_de_vendas.db")
+
+#Conectando ao banco de dados:
+Session = sessionmaker(bind=BD)
+session = Session()
+
+#Criando Tabela e Classe:
+Base = declarative_base()
+
+class Produto(Base):
+    __tablename__ = "produto"
+    
+    #Definindo variaveis da tabela:
+    nome = Column(String, primary_key=True)
+    preco = Column(Float)
+
+def __init__(self,nome:str, preco:float):
+        self.nome = nome
+        self.preco = preco
+
+#Criando tabela de dados, no banco de dados:
+Base.metadata.create_all(bind=BD)
