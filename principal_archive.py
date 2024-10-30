45906f16375ca906501e15a0e6b5d1e5fc8422b1
julia2440 <24juliavitoria@gmail.com>
2024-10-30 17:51:10 +0000

    menu_principal

--- principal_archive.py
+++ principal_archive.py
@@ -35,3 +35,22 @@ def __init__(self,nome:str, preco:float):
 
 #Criando tabela de dados, no banco de dados:
 Base.metadata.create_all(bind=BD)
+
+from os import system
+
+#limpar terminal
+
+def limpar_terminal():
+    system("cls||clear")
+
+def menu_principal():
+    limpar_terminal()
+
+    print("""
+1 - Adicionar um produto ao estoque
+2 - Alterar p preço de um produto
+3 - Renomear um produto
+4 - Remover um produto
+5 - Verificar todos os produtos no estoque
+6 - Sair do sistema
+""")
